2654bf68d0489ab09a9786030c132997f17ba513
Blake <19006041@vcconnect.co.za>
2021-10-14 17:19:16 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GreatestCommonDivisor
+{
+    class Program
+    {
+
+
+        public static int Hcf(int n1, int n2)
+        {
+            if (n2 != 0)
+                return Hcf(n2, n1 % n2);
+            else
+                return n1;
+        }
+
+        static void Main(string[] args)
+        {
+            //Using recursion to find the greatest common divisor of 2 numbers.
+            string val;
+            Console.Write("Enter First Integer: ");
+            val = Console.ReadLine();
+            int a = Convert.ToInt32(val);
+            Console.Write("Enter Second Integer: ");
+            val = Console.ReadLine();
+            int b = Convert.ToInt32(val);
+
+            int hcf = Hcf(a, b);
+            Console.Write("Greatest Common Divisor (G.C.D) of {0} and {1} is {2}.", a, b, hcf);
+            Console.ReadKey();
+        }
+    }
+}
